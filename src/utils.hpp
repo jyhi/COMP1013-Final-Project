d5ff61ff3887472cd976454506882af6e39efc65
Junde Yhi <lmy441900@aosc.xyz>
2017-12-20 11:55:17 +0800

    utils.hpp: add

---
+++ src/utils.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file utils.hpp
+ * @version 0.1
+ * @author
+ *
+ * This file contains userful utilities.
+ */
+
+#ifndef _UTILS_HPP
+#define _UTILS_HPP
+
+/**
+ * Macro STR is used to change something into a string.
+ *
+ * @see https://stackoverflow.com/questions/25410690/scanf-variable-length-specifier
+ */
+#undef _STR
+#undef STR
+#define _STR(s) #s
+#define STR(s) _STR(s)
+
+#endif /* end of include guard: _UTILS_HPP */
