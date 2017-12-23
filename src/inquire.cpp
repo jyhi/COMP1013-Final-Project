87bec7fc7800967b2f50f7b9dd19c54377230494
Zichen Luo <RainySummerLuo@users.noreply.github.com>
2017-12-23 14:39:46 +0800

    inquire_menu
    improve menu of inquire()

--- src/inquire.cpp
+++ src/inquire.cpp
@@ -2,7 +2,7 @@
 
 /**
  * @file inquire.cpp
- * @version 0.5
+ * @version 1.0
  * @author Laurence
  *
  * This file contains functions related to the Inquire option.
@@ -18,14 +18,22 @@ void inquire_by_student_id () {
 
 }
 
+void inquire_student_display () {
+
+}
+
 extern void inquire (void) {
     int inquire_menu_choice;
-
-    puts("Menu:\n");
-    puts("1. By index\n");
-    puts("2. By student id\n");
+    puts("===== Student Grading System =====\n");
+    puts("Choose an option to start:");
+    puts("Menu:");
+    puts("1. By index");
+    puts("2. By student id");
     puts("3. Quit\n");
+    printf("Your Option: ");
+    //display the menu
 
+    fflush(stdin);
     scanf("%d", &inquire_menu_choice);
 
     switch (inquire_menu_choice) {
@@ -34,7 +42,9 @@ extern void inquire (void) {
             break;
         case 2:
             inquire_by_student_id();
+            break;
         case 3:
-            return;
+        default:
+            break;
     }
 }
