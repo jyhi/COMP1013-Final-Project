6a3ed73a405581def63398712abe647835811721
Zichen Luo <RainySummerLuo@users.noreply.github.com>
2017-12-23 14:16:22 +0800

    inquire_menu
    Menu part of inquire()

--- src/inquire.cpp
+++ src/inquire.cpp
@@ -1,11 +1,40 @@
+#include <string.h>
+
 /**
  * @file inquire.cpp
- * @version 0.1
- * @author
+ * @version 0.5
+ * @author Laurence
  *
  * This file contains functions related to the Inquire option.
  */
+
 #include "node.hpp"
+
+void inquire_by_index () {
+
+}
+
+void inquire_by_student_id () {
+
+}
+
 extern void inquire (void) {
-  // Unimplemented
+    int inquire_menu_choice;
+
+    puts("Menu:\n");
+    puts("1. By index\n");
+    puts("2. By student id\n");
+    puts("3. Quit\n");
+
+    scanf("%d", &inquire_menu_choice);
+
+    switch (inquire_menu_choice) {
+        case 1:
+            inquire_by_index();
+            break;
+        case 2:
+            inquire_by_student_id();
+        case 3:
+            return;
+    }
 }
