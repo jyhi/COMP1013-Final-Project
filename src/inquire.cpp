3a2929d981dd05fab8531c59fcb8d42fda846a3a
Zichen Luo <RainySummerLuo@users.noreply.github.com>
2017-12-23 18:03:09 +0800

    inquire_read_student_marks ()
    one way to read from marks.txt
    NOT final way and incompatible to other function

--- src/inquire.cpp
+++ src/inquire.cpp
@@ -1,3 +1,5 @@
+#include "node.hpp"
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -8,22 +10,44 @@
  * This file contains functions related to the Inquire option.
  */
 
-#include "node.hpp"
-
-void inquire_by_index () {
-
-}
-
-void inquire_by_student_id () {
-
-}
-
 void inquire_student_display () {
 
 }
 
+void inquire_read_student_marks () {
+    char szTest[500] = {0};
+    bool hw_status[5] = {false};
+    Node *head, *pnode, *result;
+    FILE *fpsm = NULL;
+    read_from_marks(fpsm, &head, hw_status);
+    while(!feof(fpsm))
+    {
+        memset(szTest, 0, sizeof(szTest));
+        fgets(szTest, sizeof(szTest) - 1, fpsm); // 包含了换行符
+        printf("%s", szTest);
+    }
+    printf("\n");
+    memset(szTest, 0, sizeof(szTest));
+    fgets(szTest, sizeof(szTest) - 1, fpsm);
+    printf("%s", szTest);
+    printf("\n");
+}
+
+void inquire_by_index () {
+    inquire_read_student_marks ();
+
+    inquire_student_display ();
+}
+
+void inquire_by_student_id () {
+    inquire_read_student_marks ();
+
+    inquire_student_display ();
+}
+
 extern void inquire (void) {
-    int inquire_menu_choice;
+    //Unimplemented
+    int inquire_menu_option;
     puts("===== Student Grading System =====\n");
     puts("Choose an option to start:");
     puts("Menu:");
@@ -34,9 +58,9 @@ extern void inquire (void) {
     //display the menu
 
     fflush(stdin);
-    scanf("%d", &inquire_menu_choice);
+    scanf("%d", &inquire_menu_option);
 
-    switch (inquire_menu_choice) {
+    switch (inquire_menu_option) {
         case 1:
             inquire_by_index();
             break;
